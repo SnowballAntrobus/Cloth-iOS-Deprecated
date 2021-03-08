d0135c3b1a378b49e42187392825d87aa61d8d30
Dante Ramón Gil-Marín <drgilmarin@uchicago.edu>
2021-03-08 01:52:15 -0600

    Initial Commit

---
+++ Shared/ClothApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClothApp.swift
+//  Shared
+//
+//  Created by Dante Gil-Marin on 3/8/21.
+//
+
+import SwiftUI
+
+@main
+struct ClothApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Dante Gil-Marin on 3/8/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
