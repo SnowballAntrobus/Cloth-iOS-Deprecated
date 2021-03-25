9f61c0f9eb60439fff0c2cbc0fa2b3643b0482d8
Dante Ramón Gil-Marín <drgilmarin@uchicago.edu>
2021-03-25 18:39:59 -0500

    removed contour test

--- Shared/Views/AddView.swift
+++ Shared/Views/AddView.swift
@@ -26,7 +26,6 @@ struct AddView: View {
     @State private var selectedImage: UIImage?
     @State private var isImagePickerDisplay = false
     
-    @State var points : String = ""
     var body: some View {
         VStack {
             if selectedImage != nil {
@@ -52,9 +51,6 @@ struct AddView: View {
                                 self.sourceType = .photoLibrary
                                 self.isImagePickerDisplay.toggle()
                             }.padding()
-            Button("Detect Contours", action: {
-                            detectVisionContours()
-            }).padding()
             Button(action: { activeSheet = .second }) {
                 Image(systemName: "plus")
             }
@@ -77,52 +73,6 @@ struct AddView: View {
                     }
         }
     }
-    func detectVisionContours(){
-        _ = CIContext()
-                if let sourceImage = selectedImage
-                {
-                    let inputImage = CIImage.init(cgImage: sourceImage.cgImage!)
-                    
-                    let contourRequest = VNDetectContoursRequest.init()
-                    contourRequest.revision = VNDetectContourRequestRevision1
-                    contourRequest.contrastAdjustment = 1.0
-//                    contourRequest.maximumImageDimension = 512
-
-
-                    let requestHandler = VNImageRequestHandler.init(ciImage: inputImage, options: [:])
-
-                    try! requestHandler.perform([contourRequest])
-                    let contoursObservation = contourRequest.results?.first as! VNContoursObservation
-                    
-                    self.points  = String(contoursObservation.contourCount)
-                    self.selectedImage = drawContours(contoursObservation: contoursObservation, sourceImage: sourceImage.cgImage!)
-
-                } else {
-                    self.points = "Could not load image"
-                }
-        }
-    public func drawContours(contoursObservation: VNContoursObservation, sourceImage: CGImage) -> UIImage {
-            let size = CGSize(width: sourceImage.width, height: sourceImage.height)
-            let renderer = UIGraphicsImageRenderer(size: size)
-            
-            let renderedImage = renderer.image { (context) in
-            let renderingContext = context.cgContext
-
-            let flipVertical = CGAffineTransform(a: 1, b: 0, c: 0, d: -1, tx: 0, ty: size.height)
-            renderingContext.concatenate(flipVertical)
-
-            renderingContext.draw(sourceImage, in: CGRect(x: 0, y: 0, width: size.width, height: size.height))
-            
-            renderingContext.scaleBy(x: size.width, y: size.height)
-            renderingContext.setLineWidth(5.0 / CGFloat(size.width))
-            let redUIColor = UIColor.red
-            renderingContext.setStrokeColor(redUIColor.cgColor)
-            renderingContext.addPath(contoursObservation.normalizedPath)
-            renderingContext.strokePath()
-            }
-            
-            return renderedImage
-        }
 }
 
 struct AddView_Previews: PreviewProvider {
