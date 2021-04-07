19ae9ee58eb2179f16a68da55dbd25a0015374ec
Dante Ramón Gil-Marín <drgilmarin@uchicago.edu>
2021-04-07 14:12:11 -0500

    resetting to try firebase instead research showed it to be more promising

--- Shared/Models/User.swift
+++ Shared/Models/User.swift
@@ -6,22 +6,99 @@
 //
 
 import Foundation
-import RealmSwift
+import SwiftUI
 
-import Foundation
-import RealmSwift
-
-@objcMembers class User: Object, ObjectKeyIdentifiable {
-    dynamic var _id = UUID().uuidString
-    dynamic var partition = "" // "user=_id"
-    dynamic var userName = ""
-    dynamic var userPreferences: UserPreferences?
-    dynamic var lastSeenAt: Date?
-    var userData = UserData.data[0]
-    var clothFits = List<ClothFit>() //var conversations = List<Conversation>()
-    dynamic var presence = "Off-Line"
+class User: ObservableObject {
+    private static var documentsFolder: URL {
+        do {
+            return try FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
+        } catch {
+            fatalError("Can't find documents directory.")
+        }
+    }
+    private static var clothItemsURL: URL { return documentsFolder.appendingPathComponent("clothItems.data") }
+    private static var clothFitsURL: URL { return documentsFolder.appendingPathComponent("clothFits.data") }
+    private static var userDataURL: URL { return documentsFolder.appendingPathComponent("userData.data") }
     
-    override static func primaryKey() -> String? {
-        return "_id"
+    @Published var userData: UserData = UserData.data[0]
+    @Published var clothItems: [ClothItem] = []
+    @Published var clothFits: [ClothFit] = []
+    
+    func load() {
+        
+        DispatchQueue.global(qos: .background).async { [weak self] in
+            guard let data = try? Data(contentsOf: Self.userDataURL) else {
+                #if DEBUG
+                DispatchQueue.main.async {
+                    self?.userData = UserData.data[0]
+                }
+                #endif
+                return
+            }
+            guard let userData = try? JSONDecoder().decode(UserData.self, from: data) else {
+                fatalError("Can't decode saved cloth user data.")
+            }
+            DispatchQueue.main.async {
+                self?.userData = userData
+            }
+        }
+        
+        DispatchQueue.global(qos: .background).async { [weak self] in
+            guard let data = try? Data(contentsOf: Self.clothItemsURL) else {
+                #if DEBUG
+                DispatchQueue.main.async {
+                    self?.clothItems = ClothItem.data
+                }
+                #endif
+                return
+            }
+            guard let clothItems = try? JSONDecoder().decode([ClothItem].self, from: data) else {
+                fatalError("Can't decode saved cloth item data.")
+            }
+            DispatchQueue.main.async {
+                self?.clothItems = clothItems
+            }
+        }
+        
+        DispatchQueue.global(qos: .background).async { [weak self] in
+            guard let data = try? Data(contentsOf: Self.clothFitsURL) else {
+                #if DEBUG
+                DispatchQueue.main.async {
+                    self?.clothFits = ClothFit.data
+                }
+                #endif
+                return
+            }
+            guard let clothFits = try? JSONDecoder().decode([ClothFit].self, from: data) else {
+                fatalError("Can't decode saved cloth fit data.")
+            }
+            DispatchQueue.main.async {
+                self?.clothFits = clothFits
+            }
+        }
+    }
+    
+    func save() {
+        DispatchQueue.global(qos: .background).async { [weak self] in
+            guard let clothItems = self?.clothItems else { fatalError("Self out of scope") }
+            guard let data = try? JSONEncoder().encode(clothItems) else { fatalError("Error encoding data") }
+            do {
+                let outfile = Self.clothItemsURL
+                try data.write(to: outfile)
+            } catch {
+                fatalError("Can't write to file")
+            }
+        }
+        
+        DispatchQueue.global(qos: .background).async { [weak self] in
+            guard let clothFits = self?.clothFits else { fatalError("Self out of scope") }
+            guard let data = try? JSONEncoder().encode(clothFits) else { fatalError("Error encoding data") }
+            do {
+                let outfile = Self.clothFitsURL
+                try data.write(to: outfile)
+            } catch {
+                fatalError("Can't write to file")
+            }
+        }
     }
 }
--- Shared/Views/ClothApp.swift
+++ Shared/Views/ClothApp.swift
@@ -5,18 +5,20 @@
 //  Created by Dante Gil-Marin on 3/8/21.
 //
 
-import SwiftUI
-import RealmSwift
 
-let app = RealmSwift.App(id: "clothapp-hdgci")
+import SwiftUI
 
 @main
-struct ClothApp: SwiftUI.App {
-    @StateObject var state = AppState()
+struct ClothApp: App {
+    @ObservedObject private var user = User()
     var body: some Scene {
         WindowGroup {
-            MainView()
-                .environmentObject(state)
+            MainView(clothItems: $user.clothItems, clothFits: $user.clothFits, userData: $user.userData) {
+                user.save()
+            }
+            .onAppear {
+                user.load()
+            }
         }
     }
 }
