1f895f0e60976b63b5204f5af13675ca468eda1a
Dante Ramón Gil-Marín <drgilmarin@uchicago.edu>
2021-04-13 18:34:16 -0500

    next steps: separate user and global items and fits in closet, make landing page show at startup

--- Shared/Views/LandingView.swift
+++ Shared/Views/LandingView.swift
@@ -16,23 +16,7 @@ struct LandingView: View {
     var columns: [GridItem] = Array(repeating: .init(.flexible()), count: 3)
     
     var body: some View {
-        NavigationView {
-            VStack {
-                ScrollView {
-                    LazyVGrid(columns: columns) {
-                        ForEach(clothItemsRepo.clothItems) {clothItem in
-                            VStack {
-                                NavigationLink(
-                                    destination: ClothItemDetailView(clothItemsRepo: $clothItemsRepo, clothItem: binding(for: clothItem), clothFits: clothFitsRepo.clothFits)) {
-                                    ClothItemView(clothItem: clothItem)
-                                        .frame(width: 100, height: 100)
-                                }
-                            }
-                        }
-                    }
-                }.navigationBarHidden(true)
-            }
-        }
+        ClosetView(clothItemsRepo: $clothItemsRepo, clothFitsRepo: $clothFitsRepo, userDataRepo: $userDataRepo)
     }
     
     private func binding(for clothItem: ClothItem) -> Binding<ClothItem> {
